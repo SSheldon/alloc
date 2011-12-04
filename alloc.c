6c8ee8efb84ee7fc01b0fe9696d1a6243135b1c9
Steven Sheldon <steven.sheldon@live.com>
2011-12-04 17:51:41 +0000

    Began work on slab allocator.

--- alloc.c
+++ alloc.c
@@ -8,19 +8,77 @@
 #include <unistd.h>
 #include <string.h>
 
+#define SLABSZ 4096
+
+struct slab_data
+{
+	unsigned short int block_size;
+	unsigned short int block_count;
+	unsigned short int alloc_blocks;
+	unsigned short int first_free;
+	struct slab_data *next;
+};
+
+struct free_block
+{
+	unsigned short int next;
+	unsigned short int width;
+};
+
+void *slab_get_block(struct slab_data *slab, unsigned short int index)
+{
+	size_t offset = sizeof(struct slab_data) + index * slab->block_size;
+	return (char *)slab + offset;
+}
+
+void slab_init(struct slab_data *slab, unsigned short int block_size)
+{
+	slab->block_size = block_size;
+	slab->block_count = (SLABSZ - sizeof(struct slab_data)) / block_size;
+	slab->alloc_blocks = 0;
+	slab->first_free = 0;
+	slab->next = NULL;
+	struct free_block *block = slab_get_block(slab, 0);
+	block->next = 0;
+	block->width = slab->block_count;
+}
+
+void *slab_alloc_block(struct slab_data *slab)
+{
+	unsigned short int index = slab->first_free;
+	struct free_block *free_data = slab_get_block(slab, index);
+	if (free_data->width > 1)
+	{
+		free_data->width--;
+		slab->first_free = index + 1;
+	}
+	else slab->first_free = free_data->next;
+	slab->alloc_blocks++;
+}
+
+struct slab_data *head_slab = NULL;
+
+void *malloc(size_t size)
+{
+	if (head_slab == NULL)
+	{
+		size_t break_offset = (size_t)sbrk(0) % SLABSZ;
+		if (break_offset != 0)
+			sbrk(SLABSZ - break_offset);
+		head_slab = sbrk(SLABSZ);
+		slab_init(head_slab, 4);
+	}
+	return slab_alloc_block(head_slab);
+}
+
 void *calloc(size_t nmemb, size_t size)
 {
 	void *ptr = malloc(nmemb * size);
-	
+
 	if (ptr)
 		memset(ptr, 0x00, nmemb * size);
 
 	return ptr;
-}
-
-void *malloc(size_t size)
-{
-	return NULL;
 }
 
 void free(void *ptr)
